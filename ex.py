c56b11ce77599bc56774d2cf3abb28eb01ca6c9f
chikarcas <ferromvel@gmail.com>
2022-07-05 04:03:09 -0500

    Create some code for ploting.

--- ex.py
+++ ex.py
@@ -1,4 +1,11 @@
-msg = "Hola mundo"
-print(msg)
-print(3+4)
-print(msg, "putos")
+import matplotlib.pyplot as plt
+import numpy as np
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+x = np.linspace(-10, 10, 1000)  # Create a list of evenly-spaced numbers over the range
+plt.plot(x, sigmoid(x))       # Plot the sine of each x point
+plt.xlim(-10,10)
+plt.ylim(-0.1,1.1)
+plt.show()                   # Display the plotSet-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process
